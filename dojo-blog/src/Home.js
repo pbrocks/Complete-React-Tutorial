d3f50b7f4f26a19dc540edb67a4798680f0a53f2
Paul Barthmaier <pbrocks@users.noreply.github.com>
2022-04-05 07:14:17 -0400

    add extra blogs

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -2,22 +2,24 @@ import { useState } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-  ])
+	const [blogs, setBlogs] = useState([
+		{ title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+		{ title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+		{ title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 },
+		{ title: 'Warriors only!', body: 'lorem ipsum...', author: 'yoshi', id: 4 },
+		{ title: 'Wellness tips', body: 'lorem ipsum...', author: 'mario', id: 5 }
+	])
 
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter(blog => blog.id !== id);
-    setBlogs(newBlogs);
-  }
+	const handleDelete = (id) => {
+		const newBlogs = blogs.filter(blog => blog.id !== id);
+		setBlogs(newBlogs);
+	}
 
-  return (
-    <div className="home">
-      <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
-    </div>
-  );
+	return (
+		<div className="home">
+			<BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+		</div>
+	);
 }
- 
+
 export default Home;
